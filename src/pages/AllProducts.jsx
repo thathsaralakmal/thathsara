c7127c1b79e4d0613cf3c031d457b92265f8adcf
thathsaralakmal <thathsaralakmal8@.com>
2025-05-07 14:58:03 +0530

    project01

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -8,7 +8,7 @@ const {Products}= useAppContext()
   return (
     <div className='mt-16 flex flex-col' >
        <div className='flex flex-col items-end w-max'>
-        <p>All products</p>
+        <p className='text-2xl font-medium uppercase'>All products</p>
          <div  className='w-16 h-0.5 bg-primary rounded-full'></div>
       </div>
     </div>
